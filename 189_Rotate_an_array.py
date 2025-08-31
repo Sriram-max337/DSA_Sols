b2b5769082696f8be2dd2ea974eb4697c0d12e9e
Sriram-max337 <rohitvsriram@gmail.com>
2025-08-31 20:59:02 +0530

    189_Rotate_an_array.py

--- 189_Rotate_an_array.py
+++ 189_Rotate_an_array.py
@@ -1,7 +1,28 @@
+#Our logic Optimised - List Slicing
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         k = k % len(nums)
+
         nums[:] = nums[-k:] + nums[:-k] 
+
+#Core logic, general ways -> Array Reversal
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+    if k > len(nums):
+        k = k%len(nums)
+
+    def reverse(l,r):
+        while l<r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l+=1
+            r-=1
+
+    reverse(0,len(nums)-1)
+    reverse(0,k-1)
+    reverse(k,len(nums)-1)
