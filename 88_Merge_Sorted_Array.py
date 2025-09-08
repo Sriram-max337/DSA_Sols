b862f25f8e2b6e909305f858912621b90197ac01
Sriram-max337 <rohitvsriram@gmail.com>
2025-09-08 14:56:41 +0530

    Merge_Sorted_Array.py

--- 88_Merge_Sorted_Array.py
+++ 88_Merge_Sorted_Array.py
@@ -15,4 +15,11 @@ class Solution(object):
 
         for i in range(len(newarr)):
             nums1[i] = newarr[i]
+#One liner        
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[:] = sorted(nums1[:m] + nums2[:n])
         
