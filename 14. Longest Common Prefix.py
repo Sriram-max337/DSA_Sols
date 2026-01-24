8d9e69edcf71e5ec3c40e98e2e81b3c344568cb7
Sriram-max337 <rohitvsriram@gmail.com>
2026-01-24 23:25:10 +0530

    extra sol method for LC 14

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if len(strs)==[]:
+        if len(strs)==0:
             return
         elif len(strs)==1:
             return strs[0]
