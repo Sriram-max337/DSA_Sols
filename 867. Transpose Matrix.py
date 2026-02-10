4eb76916d951bb02274f23dd502f72bae2299896
Sriram-max337 <rohitvsriram@gmail.com>
2026-02-10 11:12:03 +0530

    LC 867 done

--- 867. Transpose Matrix.py
+++ 867. Transpose Matrix.py
@@ -1,11 +1,9 @@
-matrix = [[1,2,3],[4,5,6]]
-ans = []
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        ans = []
+        for i in range(len(matrix[0])):
+            ans.append([])
+            for j in range(len(matrix)):
+                ans[i].append(matrix[j][i])
 
-for i in range(len(matrix[0])):
-    ans.append([])
-    for j in range(len(matrix)):
-        ans[i].append(matrix[j][i])
-
-print(ans)
-
-
+        return ans
