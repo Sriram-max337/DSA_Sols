090e0cf49d133df2cafa327e0778e2f6f7d33a2c
Sriram-max337 <rohitvsriram@gmail.com>
2026-02-18 12:02:06 +0530

    LC 566 done

--- 566. Reshape the Matrix.py
+++ 566. Reshape the Matrix.py
@@ -1,7 +1,22 @@
-mat = [[1,2],[3,4]]
-r = 1
-c = 4
-m = len(mat)
-n = len(mat[0])
-eles = []
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+
+        flat_mat = []
+        upd_mat = []
+        if m*n != r*c:
+            return mat
+
+        for i in range(m*n):
+            a = i//n
+            b = i%n
+            flat_mat.append(mat[a][b])
+
+        for i in range(r):
+            upd_mat.append([])
+            for j in range(c):
+                upd_mat[i].append(flat_mat[i*c+j])
+
+        return upd_mat
 
