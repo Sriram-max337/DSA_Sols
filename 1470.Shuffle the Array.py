d7eafff9bd4a27b311202fb472718041c5a24a95
Sriram-max337 <rohitvsriram@gmail.com>
2026-02-19 22:19:33 +0530

    LC 1470 done

--- 1470.Shuffle the Array.py
+++ 1470.Shuffle the Array.py
@@ -1,15 +1,11 @@
-nums = [2,5,1,3,4,7]
-n = 3
-
-upd_nums = []
-
-p1 = 0
-p2 = n
-
-while p2 < len(nums):
-    upd_nums.append(nums[p1])
-    upd_nums.append(nums[p2])
-    p1+=1
-    p2+=1
-
-print(upd_nums)
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        upd_nums = []
+        p1 = 0
+        p2 = n
+        while p2 < len(nums):
+            upd_nums.append(nums[p1])
+            upd_nums.append(nums[p2])
+            p1+=1
+            p2+=1
+        return upd_nums
