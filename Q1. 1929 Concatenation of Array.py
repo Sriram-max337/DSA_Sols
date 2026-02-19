242a36081b700a6a819dbbfab6054b4a38662cb0
Sriram-max337 <rohitvsriram@gmail.com>
2026-02-19 21:57:28 +0530

    Updated sol of Q1 LC 1929

--- Q1. 1929 Concatenation of Array.py
+++ Q1. 1929 Concatenation of Array.py
@@ -1,10 +1,3 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans = []
-        for i in range(len(nums)):
-            ans.append(nums[i])
-
-        for j in range(len(nums)):
-            ans.append(nums[j])
-
-        return ans
+        return nums*2
