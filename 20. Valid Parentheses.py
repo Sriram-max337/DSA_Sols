506a44458f77c06475606f489c88514408a3dd6b
Sriram-max337 <rohitvsriram@gmail.com>
2026-03-06 21:23:13 +0530

    LC 20 done

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in range(len(s)):
+            if s[i] == "(" or s[i]=="[" or s[i]=="{":
+                stack.append(s[i])
+            elif stack!=[] and ((s[i] == ")" and stack[-1]=="(") or (s[i]=="]" and stack[-1]=="[") or (s[i]=="}" and stack[-1]=="{")):
+                stack.pop()
+            else:
+                return False
+        if stack == []:
+            return True
+        else:
+            return False
