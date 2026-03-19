4e2df8360477ebeff6b2a5d8031b506f4d8438a4
Sriram-max337 <rohitvsriram@gmail.com>
2026-03-19 22:05:41 +0530

    LC 134 done

--- 134. Gas Station.py
+++ 134. Gas Station.py
@@ -1,5 +1,16 @@
-gas,cost = [1,2,3,4,5],[3,4,5,1,2]
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        tank = 0
+        si = 0
 
-
-
-
+        if sum(gas) < sum(cost):
+            return -1
+        else:
+            for i in range(len(gas)):
+                tank+=gas[i]
+                tank-=cost[i]
+                if tank < 0:
+                    tank = 0
+                    si = i+1
+            return si
+        
