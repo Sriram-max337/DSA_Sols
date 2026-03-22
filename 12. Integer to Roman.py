7cb68f6271bf846d914025df1aff3a1db9265910
Sriram-max337 <rohitvsriram@gmail.com>
2026-03-22 13:07:55 +0530

    LC 12 done

---
+++ 12. Integer to Roman.py
@@ -0,0 +1,12 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman = {1:"I", 4:"IV",5:'V',9:"IX",10:'X',40:"XL",50:'L',90:"XC",100:"C",400:"CD",500:"D",900:"CM",1000:"M"}
+        num_lst = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        roman_no = ""
+
+        for n in num_lst:
+            while n <= num:
+                roman_no += roman[n]
+                num -= n
+
+        return roman_no
