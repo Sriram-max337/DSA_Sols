6f7ebbe3fd868db9b6221ad52ace20dcce7d4a54
Sriram-max337 <rohitvsriram@gmail.com>
2026-07-20 20:00:41 +0530

    LC 1081 Stack version done

--- 1081. Smallest Subsequence of Distinct Characters.py
+++ 1081. Smallest Subsequence of Distinct Characters.py
@@ -1,3 +1,5 @@
+#Direct String version
+
 class Solution(object):
     def smallestSubsequence(self, s):
         """
@@ -17,3 +19,30 @@ class Solution(object):
                     ans = ans[:-1]
                 ans+=s[i]
         return ans
+    
+#Stack version
+class Solution(object):
+    def smallestSubsequence(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        lst = []
+        HS = set()
+        HM = {}
+
+        for i in range(len(s)):
+            HM[s[i]] = i
+
+        for i in range(len(s)):
+            if s[i] in HS:
+                continue
+            else:
+                while lst and lst[-1] > s[i] and HM[lst[-1]] > i:
+                    shit = lst.pop()
+                    HS.remove(shit)
+
+                lst.append(s[i])
+                HS.add(s[i])
+
+        return "".join(lst)
