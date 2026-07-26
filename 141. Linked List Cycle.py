c3fdac6993a034aa3378c96fe30752451e5c20aa
Sriram-max337 <rohitvsriram@gmail.com>
2026-07-26 18:24:01 +0530

    LC 141 done

---
+++ 141. Linked List Cycle.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        l = head
+        r = head
+
+        while l and r and r.next:
+            l = l.next
+            r = r.next.next
+
+            if l ==  r:
+                return True
+
+        return False
