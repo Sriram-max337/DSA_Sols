5e6ad8f85b6743c5a725213448d27241a92327fa
Sriram-max337 <rohitvsriram@gmail.com>
2026-08-11 18:48:34 +0530

    LC 2996 done

--- 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum.py
+++ 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum.py
@@ -1,21 +1,14 @@
-nums = [1,2,3,2,5]
-nums = [3,4,5,1,12,14,13]
+class Solution:
+    def missingInteger(self, nums: List[int]) -> int:
+        ps = 0
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1] + 1:
+                ps += nums[i]
+            else:
+                break
 
-ps = 0
-for i in range(1,len(nums)):
-    if nums[i] == nums[i-1] + 1:
-        ps +=1 
-    else:
-        break
-
-ps += nums[0]
-m = max(nums)
-
-HS = set(nums)
-
-x = 0
-for ele in HS:
-    if ps + x in HS:
-        x+=1
-
-print(x)
+        ps += nums[0]
+        HS = set(nums)
+        while ps in HS:
+            ps += 1
+        return ps
